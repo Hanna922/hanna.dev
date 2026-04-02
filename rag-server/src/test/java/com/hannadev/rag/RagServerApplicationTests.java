2e41b5eff0bb395bf301d5fa6299ff4b9cd60d56
Hanna922 <p.ping0922@gmail.com>
2026-04-02 17:49:27 +0900

    feat: scaffold rag server

---
+++ rag-server/src/test/java/com/hannadev/rag/RagServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hannadev.rag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
